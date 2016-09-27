76b7602f9be78bd74d9587519dae4d770f3dbf2e
Trenton D Moody <tdmwashere@hotmail.com>
2016-09-26 21:35:43 -0500

    Update spotlight.bat

--- spotlight.bat
+++ spotlight.bat
@@ -7,16 +7,16 @@ mode con: cols=300 lines=78
 cls
 
 cd/
-robocopy "%root%\Users\Trenton Moody\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "C:\Users\Trenton Moody\Documents\Spotlight"
+robocopy "%root%\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "C:\Users\%USERNAME%\Documents\Spotlight"
 timeout /t 1
 cd\
 chdir Users
-chdir "Trenton Moody"
+chdir "%USERNAME%"
 chdir Documents
 chdir Spotlight
 
 Ren *.* *.jpg
 
-explorer "C:\Users\Trenton Moody\Documents\Spotlight"
-explorer "C:\Users\Trenton Moody\Pictures\Spotlight"
+explorer "C:\Users\%USERNAME%\Documents\Spotlight"
+explorer "C:\Users\%USERNAME%\Pictures\Spotlight"
 
